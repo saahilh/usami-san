e67c390650e2739e9cf186c92cf0fdecc14ded2e
Saahil Hamayun <saahil.hamayun@mail.mcgill.ca>
2021-05-05 01:57:23 -0400

    Started adding timing logic for weekly intervals

--- index.js
+++ index.js
@@ -1,55 +1,33 @@
 require('dotenv').config();
-const { Client } = require('discord.js');
+// const { createEventEmbedForDate, hiatusEmbed } = require('./constants');
+// const { Client } = require('discord.js');
+const MyTime = require('./time');
 
-const client = new Client();
+// const client = new Client();
+// console.log({ timeToMessage });
+// client.login(process.env.DISCORD_BOT_TOKEN);
 
-// const notificationTime = {
-//   year: 2021,
-//   month: 4,
-//   day: 5,
-//   hours: 19,
-// };
-
-// const { year, month, day, hours } = notificationTime;
-// const messageTime = new Date(year, month, day, hours);
-
-
-const getNextDay() {
-  const 
-}
-
-const days = {
-  monday: 0,
-  tuesday: 1,
-  wednesday: 2,
-  thursday: 3,
-  friday: 4,
-  saturday: 5,
-  sunday: 6,
+const postEventNotification = () => {
+  // TODO: implement
 };
 
-// TODO: get date of next Wednesday @ 19:00 to send the notification then instead of manually updating days
+const timer = new MyTime();
+// TODO: uncomment when done
+// timer.queueCallbackForDate(postEventNotification, 'wednesday', '19:00', true);
 
-const eventPostTime = new Date('5 May, 2021 19:00:00');
-const timeToEventPosted = eventPostTime - Date.now();
-
-const eventEmbed = {
-  "color": 0,
-  "title": ":game_die: BOARD GAME NIGHT :game_die: ",
-  "url": "https://store.steampowered.com/app/286160/Tabletop_Simulator/",
-  "author": {
-    "name": "Get Tabletop Simulator here",
-    "icon_url": "https://pbs.twimg.com/profile_images/1368668417549557761/cwmgoiJ2.png ",
-  },
-  "description": "Weekly board game night. React to let us know if you'll be there this weekend. It gives us something more to look forward to this weekend!\n\n**React in #about to let us know if you can make it!**",
-  "thumbnail": { "url": "https://i.imgur.com/chqFIZn.png" },
-  "image": { "url": "https://animananimedotcom.files.wordpress.com/2017/04/mpv-shot00014.jpg?w=636" },
-  "timestamp": eventPostTime,
-  "footer": { "text": "Location: MSAC Gaming VC on Tabletop Simulator" },
+const resetEventAndRoles = () => {
+  // TODO: implement
 };
 
+// TODO: uncomment when done
+// timer.queueCallbackForDate(resetEventAndRoles, 'sunday', '09:00', true);
+
+
+const nextEventDate = new MyTime();
+nextEventDate.advanceToDate('saturday', '20:30');
+console.log({ nextEventDate: nextEventDate.getDate() });
+// TODO: add reactions + handle reaction roles after event is created; delete reaction message from #about
+
+// const timeToEventPosted = eventPostTime - Date.now();
 // const channel =...
 // channel.send({ embed: eventEmbed });
-
-console.log({ timeToMessage });
-client.login(process.env.DISCORD_BOT_TOKEN);
---
+++ time.js
@@ -0,0 +1,64 @@
+class Time {
+  constructor(currentTime = new Date(), timezone = 'America/Toronto') {
+    this.date = new Date(currentTime.toLocaleString('en-US', { timeZone: timezone }));
+  }
+
+  static get ONE_WEEK_MS() {
+    return 86400000;
+  }
+
+  static get DAYS() {
+    return ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+  } 
+
+  getDate() {
+    return this.date;
+  }
+
+  advanceDay() {
+    this.date.setDate(this.date.getDate() + 1);
+  }
+
+  advanceToHour(hour, minute = 0) {
+    if (this.date.getHours() > hour) {
+      this.advanceDay();
+    }
+
+    this.date.setHours(hour, minute, 0, 0);
+    return this.date;
+  }
+
+  // Stays at current day if match
+  advanceToDay(dayString) {
+    const targetDayNum = Time.DAYS.indexOf(dayString.toLowerCase());
+
+    while (this.date.getDay() !== targetDayNum) {
+      this.advanceDay();
+    }
+
+    return this.date;
+  }
+
+  advanceToDate(dayString, timeString='00:00') {
+    const [hour, minute] = timeString.split(':').map((num) => parseInt(num));
+    this.advanceToHour(hour, minute);
+    this.advanceToDay(dayString);
+    return this.date;
+  }
+
+  queueCallbackForDate(callback, dayString, timeString = '00:00', repeatWeekly) {
+    const originalTime = new Time(this.date);
+    
+    const targetTime = new Time();
+    targetTime.advanceToDate(dayString, timeString);
+
+    const timeToNextCallback = targetTime - originalTime;
+    
+    setTimeout(() => {
+      setInterval(callback, Time.ONE_WEEK_MS);
+      callback();
+    }, timeToNextCallback);
+  }
+}
+
+module.exports = Time;
