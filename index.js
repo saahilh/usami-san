57ccfb46619ca40c63bf5c96913c3f3b8e4ed4b3
Saahil Hamayun <saahil.hamayun@mail.mcgill.ca>
2021-05-08 20:38:42 -0400

    Updated logic for marking members interested & for clearing event roles

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const { Client } = require('discord.js');
 const MyTime = require('./time');
 // TODO: add unit tests
 
-const client = new Client();
-
-const EMOTE_LIST = ['🟩', '🟦', '🟧', '🟥'];
-
 const EMOTE_TO_ROLE_ID = {
   '🟩': process.env.COMING_ROLE_ID,
   '🟦': process.env.PROBABLY_COMING_ROLE_ID,
@@ -15,10 +11,54 @@ const EMOTE_TO_ROLE_ID = {
   '🟥': process.env.BUSY_ROLE_ID,
 };
 
+const EMOTE_LIST = Object.keys(EMOTE_TO_ROLE_ID);
+const EMOTE_ROLE_ID_LIST = Object.values(EMOTE_TO_ROLE_ID);
+
+const client = new Client();
+
 const logMessage = (message) => {
   const date = new Date().toLocaleString('en-US', { timeZone: 'America/Toronto' });
   return console.log(`${date}: ${message}`);
 };
+
+const getActiveMemberList = async (memberList) => {
+  const activeMemberList = [];
+
+  const memberIsActive = async (member) => member.user.bot || await member.roles.cache.has(process.env.HIATUS_ROLE_ID);
+
+  const addMemberIfActive = async (member) => {
+    if (!(await memberIsActive(member))) return;
+
+    activeMemberList.push(member);
+  }
+
+  const getMembersPromises = [];
+  for (const member of memberList) {
+    getMembersPromises.push(addMemberIfActive(member));
+  }
+
+  return Promise.all(getMembersPromises).then(() => activeMemberList);
+}
+
+const asyncMutativeMap = async (itemList, asyncCallback) => {
+  const promiseList = [];
+
+  for (const item of itemList) {
+    const promise = asyncCallback(item);
+    promiseList.push(promise);
+  }
+  
+  return Promise.all(promiseList);
+}
+
+const markMemberInterested = async (member) => member.roles.add(process.env.INTERESTED_ROLE_ID);
+const markMembersInterested = async (memberList) => asyncMutativeMap(memberList, markMemberInterested);
+
+const makeRemoveRole = async (memberList) => async (role) => {
+  const removeRole = async (member) => member.roles.remove(role);
+  return asyncMutativeMap(memberList, removeRole);
+};
+const clearEventRoles = async (memberList, roleList=EMOTE_ROLE_ID_LIST) => asyncMutativeMap(roleList, makeRemoveRole(memberList));
 
 async function postEvent () {
   const eventDate = new MyTime();
@@ -30,7 +70,7 @@ async function postEvent () {
     await client.login(process.env.DISCORD_BOT_TOKEN);
     const server = await client.guilds.cache.get(process.env.SERVER_ID);
 
-    const eventChannel = await client.channels.cache.get(process.env.CHANNEL_ID);
+    const eventChannel = await server.channels.cache.get(process.env.CHANNEL_ID);
     const postedEmbed = await eventChannel.send({ embed: eventEmbed });
     
     const postedReactionList = [];
@@ -39,25 +79,6 @@ async function postEvent () {
       postedReactionList.push(postedReaction);
     }
     
-    const clearAllEventRoles = async () => {
-      const clearRoles = [];
-
-      // TODO: verify this works
-      await server.members.fetch();
-      for (let member of Object.values(server.members.cache)) {
-        // TODO: only do this if member is not on hiatus & is not a bot
-        // console.log(member.roles.cache.has(process.env.HIATUS_ROLE_ID));
-
-        // TODO: uncomment after making sure this work
-        // clearRoles.push(member.roles.remove(EMOTE_TO_ROLE_ID[postedReaction.emoji.name]));
-        // clearRoles.push(member.roles.add(process.env.INTERESTED_ROLE_ID));
-      }
-
-      if (!clearRoles.length) return;
-      
-      await Promise.all(...clearRoles);
-    }
-
     const filter = (reaction) => {
       return EMOTE_LIST.indexOf(reaction.emoji.name) !== -1;
     };
@@ -82,6 +103,7 @@ async function postEvent () {
 
     collector.on('remove', async (removedReaction, user) => {
       const member = await server.members.cache.get(user.id);
+      
       member.roles.remove(EMOTE_TO_ROLE_ID[removedReaction.emoji.name]);
       member.roles.add(process.env.INTERESTED_ROLE_ID);
 
@@ -89,13 +111,18 @@ async function postEvent () {
     });
 
     collector.on('end', async () => {
-      await clearAllEventRoles();
+      const memberList = await server.members.fetch();
+      const activeMemberList = await getActiveMemberList(memberList);
+      
+      // await clearEventRoles(activeMemberList);
+      // await markMembersInterested(activeMemberList);
+
       client.destroy();
 
       logMessage('Event ended & member roles cleared.');
     });
   
-    // MyTime.queueCallbackForDate(clearAllEventRoles, 'sunday', '09:00', true);
+    // MyTime.queueCallbackForDate(() => clearAllEventRoles(server), 'sunday', '09:00', true);
 
     // const sendEventNotification = () => {
     //   // TODO: implement
